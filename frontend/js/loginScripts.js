989a59d8900d4361d8999ac32c2883b4e6112aa2
Henrik <henrikhl131@gmail.com>
2020-11-22 17:42:50 -0500

    completed most scripts, still needs scripting to communicate with server and forget password page as well its scripts

--- frontend/js/loginScripts.js
+++ frontend/js/loginScripts.js
@@ -7,7 +7,15 @@ function redirectToSignup(){
     window.location.replace("signup.html");
 }
 
-//implement later
 function accLogIn(){
-    
+    document.getElementById("alertBox").innerHTML = "";
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == ""){
+        document.getElementById("alertBox").innerHTML += "Username field cannot be empty!";
+    }
+    if (password == ""){
+        document.getElementById("alertBox").innerHTML += "<br>Password field cannot be empty!";
+    }
+    //send login request to server and obtain return code
 }
