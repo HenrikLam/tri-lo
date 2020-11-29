f5480b93896a28cee235c94897248cf990a1d705
Henrik <henrikhl131@gmail.com>
2020-11-28 21:50:10 -0500

    dynamically generated html for members and kick buttons for the members

--- frontend/js/group.js
+++ frontend/js/group.js
@@ -1,19 +1,27 @@
 function doOnLoad(){
     changeProfileButton();
     loadGroupMembers();
-    checkIfOwner();
+    doIfOwner();
 }
 function loadGroupMembers(){
     var groupMembers = getGroupMembers();
     var groupList = document.getElementById("groupList");
     var groupOwner = getGroupOwner();
-    groupList.innerHTML = "<li class= \"list-group-item\"> <img src=\""+ groupOwner[0] +"\" style= \"float: left; height: 50px; width: 50px; margin-right: 10%;\">"+ groupOwner[1] + " (Owner)</li>";
+    var htmlString = "<li class= \"list-group-item\"> <img src=\""+ groupOwner[0] +"\" style= \"float: left; height: 50px; width: 50px; margin-right: 10%;\">"+ groupOwner[1] + " (Owner)</li>";
     for (var i = 0; i < groupMembers.length; i++){
-        groupList.innerHTML += "<li class= \"list-group-item\"> <img src=\""+ groupMembers[i][0] +"\" style= \"float: left; height: 50px; width: 50px; margin-right: 10%;\">"+ groupMembers[i][1] + "</li>";
+        htmlString += "<li class= \"list-group-item\"> <img src=\""+ groupMembers[i][0] +"\" style= \"float: left; height: 50px; width: 50px; margin-right: 10%;\">"+ groupMembers[i][1];
+        if (isOwner()){
+            htmlString += "<button type=\"button\" class=\"btn btn-danger\" style=\"position:absolute; right:0;margin-right:2%;\" onclick=\"kickMember("+i+")\"> Kick </button>";
+        }
+        htmlString += "</li>";
     }
 
-}
 
+    groupList.innerHTML = htmlString;
+}
+function kickMember(memberNumber){
+    console.log(memberNumber + " got kicked! D:\n");
+}
 function getGroupMembers(){
     return [["sisman.png","JaneDoe"], ["sisman.png", "JohnDough"], ["sisman.png", "DoughDoughnut"]];
 }
@@ -35,8 +43,11 @@ function changeProfileButton(){
     document.getElementById("profileNavButton").innerHTML = "<img src =\"" + getProfilePicture() + "\" class = \"rounded-circle\" style = \"height:40px; width:40px\"> " + getUsername();
 }
 
-function checkIfOwner(){
-    if (getGroupOwner()[1] == getUsername()){
+function isOwner(){
+    return (getGroupOwner()[1] == getUsername());
+}
+function doIfOwner(){
+    if (isOwner()){
         document.getElementById("addMemberDiv").innerHTML = "<button type=\"button\" class=\"btn btn-primary\" style=\"margin-bottom: 2%;\"> Add Member</button>";
     }
 }
