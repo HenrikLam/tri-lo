ab7a6a2e1816ca1a775e770390c4f7a97800cc43
Javier Portorreal <portoj@rpi.edu>
2020-11-29 22:59:14 -0500

    sample data

--- database/new_setup.sql
+++ database/new_setup.sql
@@ -124,7 +124,7 @@ SET GLOBAL local_infile = 'ON';
 
 LOAD DATA INFILE '..\\..\\htdocs\\tri-lo\\database\\listings.csv' INTO TABLE listings
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n' (listingId,address,city,state,zipcode,latitude,longitude,price,bathrooms,bedrooms,squareFeet,isRenting,paymentFrequency,status,ownerId,dateTimePosted);
+LINES TERMINATED BY '\n' (listingId,address,city,state,zipcode,latitude,longitude,price,bathrooms,bedrooms,squareFeet,isRenting,paymentFrequency,status,ownerId,dateTimePosted,listingName);
 
 LOAD DATA INFILE '..\\..\\htdocs\\tri-lo\\database\\amenities.csv' INTO TABLE listingAmenities
 FIELDS TERMINATED BY ','
@@ -138,7 +138,85 @@ LOAD DATA INFILE '..\\..\\htdocs\\tri-lo\\database\\images.csv' INTO TABLE image
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n' (imageId, listingId, link);
 
-INSERT INTO users
-VALUES (1, 'Javier', 'Portorreal', 'portoj', 'Password123', 'portoj@rpi.edu');
+INSERT INTO users VALUES 
+(1, 'Land', 'Lord', 'lordl1', 'Password123', 'lordl1@rpi.edu'),
+(2, 'Land', 'Lord', 'lordl2', 'Password123', 'lordl2@rpi.edu'),
+(3, 'Land', 'Lord', 'lordl3', 'Password123', 'lordl3@rpi.edu'),
+(4, 'RealEstate', 'Agent', 'agentr', 'Password123', 'agentr@rpi.edu'),
+(5, 'Basic', 'User', 'username', 'Password123', 'user@rpi.edu'),
+(6, 'Joe', 'Biden', 'bidenj', 'Password123', 'bidenj@rpi.edu'),
+(7, 'Donald', 'Trump', 'trumpd', 'Password123', 'trumpd@rpi.edu'),
+(8, 'Hillary', 'Clinton', 'clinth', 'Password123', 'clinth@rpi.edu'),
+(9, 'Bernie', 'Sanders', 'sandeb', 'Password123', 'sandeb@rpi.edu'),
+(10, 'John', 'Smith', 'smithj', 'Password123', 'smithj@rpi.edu'),
+(11, 'Beth', 'Smith', 'smithb', 'Password123', 'smithb@rpi.edu'),
+(12, 'Samantha', 'Cross', 'crosss', 'Password123', 'crosss@rpi.edu');
+
+INSERT INTO `groups` VALUES
+(1, 'Group A', 'Looking for a cute apartment', 5),
+(2, 'Group B', 'Delete Me', 5),
+(3, 'Group C', 'User Invited', 10),
+(4, 'Group D', 'Other', 12);
+
+INSERT INTO `groupMembers` VALUES 
+(1, 5),
+(1, 6),
+(1, 7),
+(2, 5),
+(2, 10),
+(3, 10),
+(3, 11),
+(3, 12),
+(4, 11),
+(4, 12);
+
+INSERT INTO `groupInvitations` VALUES 
+(1, 8, 5),
+(1, 9, 5),
+(2, 9, 12),
+(3, 5, 10);
+
+INSERT INTO `collections` VALUES
+(1, "Big Bookmark", 5),
+(2, "Empty", 5),
+(3, "Favorites 1", 5),
+(4, "Favorites 2", 5),
+(5, "Favorites 3", 5);
+
+INSERT INTO `collectionListings` VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(1, 7),
+(1, 8),
+(1, 9),
+(1, 10),
+(1, 11),
+(1, 12),
+(1, 13),
+(1, 14),
+(1, 15),
+(1, 16),
+(1, 17),
+(1, 18),
+(1, 19),
+(1, 20),
+(1, 21),
+(1, 22),
+(1, 23),
+(1, 24),
+(1, 25),
+(3, 5),
+(3, 11),
+(3, 27),
+(4, 8),
+(4, 9),
+(4, 44),
+(5, 1),
+(5, 50),
+(5, 51);
 
 -- Alternative: mysqlimport --local --verbose --delete --columns=id,name,alt_name,iata,icao,callsign,country,active --fields-terminated-by=\, --fields-optionally-enclosed-by=\" --lines-terminated-by=\n --user=Cony -p openflight "c:\ProgramData\MySQL\MySQL Server 8.0\Uploads\airlines.csv"
