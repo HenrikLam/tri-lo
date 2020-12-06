55d6403a832829cce9c225508606178e26ed2cd1
Javier Portorreal <portoj@rpi.edu>
2020-12-06 14:32:07 -0500

    listing images in slideshow

--- frontend/js/exlistpageScript.js
+++ frontend/js/exlistpageScript.js
@@ -42,8 +42,25 @@ function display() {
 	var meme = Object.keys(data['amenities']);
 	meme.forEach(element => document.getElementById("amen").innerHTML += (element + ": " + data['amenities'][element] + "<br>"));
 
-	document.getElementById("im1").src = data['imageLink'];
-	document.getElementById("im2").src = data['imageLink'];
+  for (var i = 1; i <= data.numImages; i++) {
+    var div = document.createElement("div");
+    div.classList.add("carousel-item");
+
+    if (i == 1)
+      div.classList.add("active");
+
+    var image = document.createElement("img");
+    image.classList.add("d-block");
+    image.classList.add("w-100");
+    image.id = "im" + i;
+    image.alt = "slide " + i;
+    image.src = data.images[i];
+
+    div.appendChild(image);
+    document.getElementById("carousel-container").appendChild(div);
+  }
+
+  // lower buttons
 	document.getElementById("img1").src = data['imageLink'];
 	document.getElementById("img2").src = data['imageLink'];
 }
