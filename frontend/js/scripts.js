4f2f0f94985e597c2e76829c35591190116cd468
HenrikLam <henrikhl131@gmail.com>
2020-12-07 18:15:29 -0500

    added create listing button to navbar

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -2,10 +2,15 @@ function setEventListeners(){
     //alert("Debug Message");
     document.getElementById("home").addEventListener("click", redirectToHome);
     document.getElementById("login").addEventListener("click", redirectToLogin);
+    document.getElementById("createListing").addEventListener("click",redirectToCreate);
 }
 function redirectToHome(){
     window.location.replace("homepage.html");
 }
 function redirectToLogin(){
     window.location.replace("login.html");
+}
+function redirectToCreate(){
+    console.log("hi");
+    window.location.replace("listing_create.html");
 }
